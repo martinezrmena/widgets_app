c4e955af39a1ef1ce2280112edd52d5c4d2c52c4
Rafael Martinez <rafaelalfon@hotmail.com>
2024-05-08 20:08:56 -0600

    Snackbars

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,30 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: (){}),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y Dialogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        label: const Text('Mostrar Snackbar'),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
